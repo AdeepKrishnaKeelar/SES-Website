6cee9bc36316207aa0ced3df5e478d3380a06ef5
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-02-27 20:46:08 +0530

    Changes made, and added login page

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sampaje Education Society</title>
+</head>
+<body>
+    <section class="banner" id="banner">
+        <div class="contactForm">
+            <form method="post">
+                <div class="inputBox">
+                    <input type="text" name="Name" id="Name" placeholder="Name" required>
+                </div>
+                <div class="inputBox">
+                    <input type="text" name="Register" id="Register" placeholder="Register" required>
+                </div>
+                <div class="inputBox">
+                    <input type="date" name="DOB" id="DOB" placeholder="DOB" required>
+                </div>
+                <div class="inputBox">
+                    <input type="submit" id="Submit" value="Send">
+                </div>
+            </form>
+        </div>
+    </section>    
+</body>
+</html>
